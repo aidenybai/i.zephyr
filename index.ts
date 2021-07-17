3d25551e9eff1fcfad9665bb4cadac07e94792b3
Will Lane <williamlane923@gmail.com>
2021-07-17 00:22:19 -0400

    Basic image downloader

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
+import util from 'util';
+import fs from 'fs';
+import path from 'path';
+import { pipeline } from 'stream';
+import fastifyMultiPart from 'fastify-multipart';
+const pump = util.promisify(pipeline);
 
 const server = fastify()
 
+server.register(fastifyMultiPart);
+
 server.get('/ping', async (request, reply) => {
   return 'pong\n'
+})
+
+server.post('/', async (req, rep) => {
+  const data = await req.file();
+  await pump(data.file, fs.createWriteStream(data.filename));
+  rep.send();
 })
 
 server.listen(8080, (err, address) => {
