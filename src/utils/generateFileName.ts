7cc9a06ac56cc1435eff63fa9cffd4eab4d4a3dc
Will Lane <williamlane923@gmail.com>
2021-07-17 01:09:45 -0400

    utils

--- src/utils/generateFileName.ts
+++ src/utils/generateFileName.ts
@@ -1,7 +1,5 @@
 import { nanoid } from 'nanoid';
 
-const generateFileName = (fileName: string): string => {
-  return [...fileName.split('.'), nanoid(5)].join('-');
+export default (fileName: string): string => {
+  return fileName.replace(/(?:\.(?![^.]+$)|[^\w.])+/g, "-") + `.${nanoid()}`;
 };
-
-export default generateFileName;
