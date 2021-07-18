6b30f737619e6e9b028bffb17f6ffe7211a8f5f0
Will Lane <williamlane923@gmail.com>
2021-07-17 23:16:16 -0400

    removed target folder

--- i-cli/target/debug/deps/i_cli-2a4f69bfc82d7743.d
+++
@@ -1,5 +0,0 @@
-/Users/willlane/dev/i.zephyr/i-cli/target/debug/deps/i_cli-2a4f69bfc82d7743.rmeta: src/main.rs
-
-/Users/willlane/dev/i.zephyr/i-cli/target/debug/deps/i_cli-2a4f69bfc82d7743.d: src/main.rs
-
-src/main.rs:
